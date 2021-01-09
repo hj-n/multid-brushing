670c7fd59309ab42df373e6e15c1bc3790921c89
Hyeon Jeon <hj@hcil.snu.ac.kr>
2021-01-09 21:09:47 +0900

    set basic server setting for implementing the application

---
+++ server/brushing/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BrushingConfig(AppConfig):
+    name = 'brushing'
---
+++ server/brushing/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def density(request):
+    return HttpResponse("Hello Hello")
