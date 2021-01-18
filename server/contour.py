6d662126b38fecd7f30fa2797262b8fddf8c3f76
Hyeon Jeon <hj@hcil.snu.ac.kr>
2021-01-18 17:38:19 +0900

    skeleton code for contour generation implemented

---
+++ server/contour.py
@@ -0,0 +1,14 @@
+'''
+The code for generating the contour of current selection
+
+kernel_density: performs 2d histogram-based approximated kde (HAKDE)
+marching_squares: perform marching squares algorithm for final contour computation
+
+
+'''
+
+def kernel_density():
+    pass
+
+def marching_squares():
+    pass
--- server/server.py
+++ server/server.py
@@ -6,6 +6,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pathlib import Path
 
+import contour as Contour
+
 app = Flask(__name__)
 CORS(app)
 
