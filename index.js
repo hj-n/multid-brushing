7167d8f28ccabc654d0b266c285b49481bcf66b1
Hyeon Jeon <hj@hcil.snu.ac.kr>
2024-01-31 18:22:46 -0500

    need to render dots in canvas

--- index.js
+++ index.js
@@ -1,27 +1,65 @@
 import { csrTo2DArray } from "./utils/csrParser";
+import * as pr from "./utils/pointRenderer";
 
 class MultiDBrushing {
 	
-	constructor(preprocessed, technique, canvasDom, size) {
+	constructor(
+		preprocessed, 
+		technique, 
+		canvasDom, 
+		canvasSize, 
+		pointSize,
+		pointRenderingStyle
+	) {
 		this.preprocessed = preprocessed;
 		this.technique = technique;
 		this.canvasDom = canvasDom;
-		this.size = size;
+		this.canvasSize = canvasSize;
+		this.pointSize = pointSize;
+		this.pointRenderingStyle = pointRenderingStyle;
+
+		// set context of the canvas
+		this.ctx = canvasDom.getContext("2d");
+
 
 		this.parser();
+		this.initialRendering();
 	}
 
 	parser() {
 		/**
-		 * This function will parse the preprocessed data and re-organize it to be suitable for JS manipulation
+		 * parse the preprocessed data and re-organize it to be suitable for JS manipulation
 		 */
 		const csr = this.preprocessed.csr;
-		this.hdSim = csrTo2DArray(csr);
 
-		console.log(this.hdSim);
+		// Important informations
+		this.hdSim = csrTo2DArray(csr);
+		this.hd = this.preprocessed.hd;
+		this.ld = this.preprocessed.ld;
+		this.knn = this.preprocessed.knn;
+		this.labels = this.preprocessed.labels;
+	}
+
+
+	initialRendering() {
+		/**
+		 * Render the initial points
+		 */
+		pr.render(
+			this.pointRenderingStyle,
+			this.canvasDom,
+			this.ctx,
+			this.ld,
+			"black",
+			1,
+			this.canvasSize,
+			this.pointSize
+		);
 
 
 	}
+
+	
 
 }
 
---
+++ utils/pointRenderer.js
@@ -0,0 +1,49 @@
+import * as d3 from 'd3';
+
+
+function scalePoints(points, canvasSize) {
+	/**
+	 * Scales the points to fit the canvas size
+	 * Left 10% for padding
+	*/
+	const padding = 0.1;
+	const width = canvasSize;
+	const height = canvasSize;
+
+	const xDomain = d3.extent(points, d => d[0]);
+	const yDomain = d3.extent(points, d => d[1]);
+
+	const xScale = d3.scaleLinear().domain(xDomain).range([padding * width, width - padding * width]);
+	const yScale = d3.scaleLinear().domain(yDomain).range([padding * height, height - padding * height]);
+
+	return points.map(p => [xScale(p[0]), yScale(p[1])]);
+}
+
+export function render(renderingStyle, canvasDom, ctx, ld, color, opacity, canvasSize, pointSize) {
+	const ld_scaled = scalePoints(ld, canvasSize);
+	// if  not an array, convert it to array
+	if (!Array.isArray(color)) { color = [color] * ld_scaled.length; }
+	if (!Array.isArray(opacity)) { opacity = [opacity] * ld_scaled.length; }
+	if (!Array.isArray(pointSize)) { pointSize = [pointSize] * ld_scaled.length; }
+
+	if (renderingStyle === "dot") {
+		dotRender(canvasDom, ctx, ld_scaled, color, opacity, canvasSize, pointSize);
+	}
+}
+
+export function dotRender(canvasDom, ctx, ld, color, opacity, canvasSize, pointSize) {
+	/**
+	 * Render the points as dots
+	 */
+	console.log(ld);
+	ctx.clearRect(0, 0, canvasSize, canvasSize);
+	for (let i = 0; i < ld.length; i++) {
+		console.log("TEXT")
+		ctx.beginPath();
+		ctx.arc(ld[i][0], ld[i][1], pointSize[i], 0, 2 * Math.PI);
+		ctx.fillStyle = color[i];
+		ctx.globalAlpha = opacity[i];
+		ctx.fill();
+	}
+
+}
